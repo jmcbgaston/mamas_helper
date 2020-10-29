e959cec31dcf79915f3b77a47f1280443d4c28f5
Jason Zhen <jasonzhen.mail@gmail.com>
2020-10-29 15:37:12 -0700

    remove unneeded h1 tag from msg subject

--- routes/api/emails.js
+++ routes/api/emails.js
@@ -11,7 +11,7 @@ router.post('/new', (req, res) => {
   const msg = {
     to: req.body.email,
     from: keys.verifiedSender,
-    subject: `<h1>Mama's Helper: Daily Tasks for ${req.body.handle}</h1>`,
+    subject: `Mama's Helper: Daily Tasks for ${req.body.handle}`,
     html: req.body.html
   }
 
