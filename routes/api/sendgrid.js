f0f53a1fab93e6a6b719483756cd0fb4d1a92728
Jason Zhen <jasonzhen.mail@gmail.com>
2020-10-29 10:09:17 -0700

    Update sengrid backend route to depend on req body

--- routes/api/sendgrid.js
+++ routes/api/sendgrid.js
@@ -5,28 +5,24 @@ const keys = require('../../config/keys');
 const User = require('../../models/User');
 const Task = require('../../models/Task');
 
-router.get("/test", (req, res) => res.json({ msg: "This is the sendgrid route" }));
-
 router.post('/', (req, res) => {
-
   sgMail.setApiKey(keys.sendGridApiKey);
-  const receiver = 'una.schmitt50@ethereal.email';
 
   const msg = {
-    to: receiver, // Change to your recipient
-    from: keys.verifiedSender, // Change to your verified sender
-    subject: "Mama's Helper: Daily Tasks",
-    text: 'Theses are your tasks with requirements for the day.',
-    html: '<strong>Theses are your tasks with requirements for the day.</strong>',
+    to: req.body.email,
+    from: keys.verifiedSender,
+    subject: `Mama's Helper: Daily Tasks for ${req.body.handle}`,
+    html: req.body.html
   }
 
   sgMail
   .send(msg)
-  .then((res) => {
-    console.log('Email sent');
+  .then(() => {
+    res.json(msg);
   })
   .catch((err) => {
-    console.error(err);
+    const { message, code, response: { body: { errors }}} = err;
+    res.json({ message, code, errors });
   })
 })
 
