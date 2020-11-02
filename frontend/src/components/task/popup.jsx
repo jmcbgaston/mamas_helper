85671b88c84d6adc981766c1b05363e87925ce44
Daniel Ackroyd_Isales <dackroydisales@gmail.com>
2020-11-01 19:19:00 -0800

    Edit text

--- frontend/src/components/task/popup.jsx
+++ frontend/src/components/task/popup.jsx
@@ -6,7 +6,7 @@ class Popup extends React.Component {
         return (
             <div className="popup">
                 <div className="inner-popup">
-                    <h1 className="modal-text">To Add Requirements, please click on the Task and then click Edit Task. Sorry for the inconvenience.  </h1>
+                    <h1 className="modal-text">Your Task has been added! To add Requirements to this task, please select the Task and then select Edit Task. </h1>
                     <button onClick={this.props.closePopup}>X</button>
                 </div>
             </div>
