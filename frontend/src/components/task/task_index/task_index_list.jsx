5cda12e2d39035c0c5f8904eb39b70b14a8da58c
Jason Zhen <jasonzhen.mail@gmail.com>
2020-11-09 16:07:56 -0800

    change task list modal button color

--- frontend/src/components/task/task_index/task_index_list.jsx
+++ frontend/src/components/task/task_index/task_index_list.jsx
@@ -1,7 +1,10 @@
+import React from 'react';
+import CloseIcon from '@material-ui/icons/Close';
+
 const TaskIndexListItem = ({task, requirements}) => {
   return (
     <li>
-      <p className="task-list-item__header">{task.title}</p>
+      <p className="task-index__task-list-item-header">{task.title}</p>
       <ul>
         {requirements.map((requirement) => {
           return <li className="task-list-item__requirement">{requirement.description}</li>
@@ -16,21 +19,21 @@ export const TaskIndexList = ({tasks, checkedTasksIds, handleClose}) => {
   .filter((taskId) => checkedTasksIds[taskId]);
 
   return (
-    <div className="modal">
-      <div className="modal-main">
-        <div className="button-container">
-          <button className="modal-header__button" onClick={handleClose}>
-            <i class="far fa-window-close fa-3x" />
+    <div className="task-index__task-list-container">
+      <div className="task-index__task-list">
+        <div className="task-index__task-list-button-container">
+          <button class="task-index__task-list-button" onClick={handleClose}>
+             <CloseIcon/>
           </button>
-          <span className="modal-header__title">Task List</span>
+          <span className="task-index__task-list-header">Task List</span>
         </div>
-        <ul className="modal-items-container">
+        <ul className="task-index__task-list-items-container">
           {checked.map((taskId, idx) => {
             const task = tasks.find((requirement) => requirement._id === taskId);
             return (
               <>
                 <TaskIndexListItem task={task} requirements={task.requirements} />
-                {(checked.length > 1 && idx !== checked.length - 1) ? <div className="modal-items-spacing"/> : null}
+                {(checked.length > 1 && idx !== checked.length - 1) ? <div className="task-index__task-list-items--spacing"/> : null}
               </>
             )
           })}
