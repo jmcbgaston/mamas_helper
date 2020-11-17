ac77658d4488d26c9788cd978a7dd5bde9ee1a4c
Daniel Ackroyd_Isales <dackroydisales@gmail.com>
2020-11-17 14:34:38 -0800

    Add logo and favicon

--- frontend/src/components/nav/navbar.js
+++ frontend/src/components/nav/navbar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom'
 import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+import logo from './PNG_MH_Logo.png';
 
 const AuthNav = ({ logout }) => (
   <>
@@ -40,8 +41,7 @@ const NavBar = (props) => {
   return (
     <nav className="nav-bar">
       <Link to="/" className="nav-bar__app-title">
-        <p className="nav-bar__logo">(logo)&nbsp;</p>
-        Mama's Helper
+        <img className="nav-bar__logo" src={logo}/>
       </Link>
 
       { loggedIn ? <AuthNav logout={logout}/> :<UnAuthNav /> }
